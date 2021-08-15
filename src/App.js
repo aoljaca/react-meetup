53a734b3214a0a7003062ec10aeab0d9aea45c90
Andrej <andrejjj222@gmail.com>
2021-08-15 09:59:40 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import HomeBody from './components/HomeBody';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <HomeBody />
     </div>
   );
 }
--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -1,14 +1,44 @@
-import { Box, Button } from '@material-ui/core'
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import { Button } from '@material-ui/core';
 
-function HomeBody() {
-    return (
-        <div>
-            <Box component="span" m={1}>
-  <Button />
-</Box>
-        </div>
-    )
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign: 'center',
+  }
+}));
+
+export default function HomeBody() {
+  const classes = useStyles();
+  const ageGroups = [
+      "18-22",
+      "22-24",
+      "24-26",
+      "26-28",
+      "29-30",
+  ]
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3} className={classes.container}>
+        {ageGroups.map((age, index) => (
+            <Grid item xs={12} container className={classes.container}>
+                <Button>{age}</Button>
+            </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 }
-
-export default HomeBody
