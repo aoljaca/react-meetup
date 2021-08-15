96ec7055941ec7d10e5900e471341fffda95cfea
Andrej <andrejjj222@gmail.com>
2021-08-15 08:54:36 -0400

    c

---
+++ src/components/HomeBody.js
@@ -0,0 +1,14 @@
+import { Box, Button } from '@material-ui/core'
+import React from 'react'
+
+function HomeBody() {
+    return (
+        <div>
+            <Box component="span" m={1}>
+  <Button />
+</Box>
+        </div>
+    )
+}
+
+export default HomeBody
